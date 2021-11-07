1aec2a64c219829a12c90b52b02a1ca305711ef4
vania-sgn <Skladchikov.vania@gmail.com>
2021-11-07 23:17:55 +0300

    партия 1

---
+++ src/Part2/november_7.java
@@ -0,0 +1,8 @@
+package Part2;
+
+public class november_7 {
+    public static void main(String[] args) {
+        System.out.println("Привет Ванек");
+        System.out.println("Привет Ванек");
+    }
+}
