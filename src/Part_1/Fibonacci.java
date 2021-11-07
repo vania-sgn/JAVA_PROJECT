a66a1c9cd110ffb3caf3fdc3396d08a3d7aca9cf
vania-sgn <Skladchikov.vania@gmail.com>
2021-11-07 23:11:11 +0300

    Initial commit

---
+++ src/Part_1/Fibonacci.java
@@ -0,0 +1,30 @@
+/*package Part_1;
+
+public class Fibonacci {
+    public static void main(String[] args) {
+        int lo=1;
+        int hi=1;
+        System.out.println (lo);
+        while (hi<50){
+            System.out.println(hi);
+            hi=lo+hi;
+            lo=hi-lo;
+
+        }
+    }
+}*/
+package Part_1;
+
+public class Fibonacci {
+    public static void main(String[] args) {
+        int lo=2;
+        int hi=4;
+        System.out.println (lo);
+        while (hi<500){
+            System.out.println(hi);
+            hi=lo*hi;
+            lo=hi/lo;
+
+        }
+    }
+}
---
+++ src/Part_1/Vvod1_1.java
@@ -0,0 +1,7 @@
+package Part_1;
+
+public class Vvod1_1 {
+    public static void main(String[] args) {
+        System.out.printf("111");
+    }
+}
