93c92647a4ff9b545abcc76df3605f46615c842c
vania-sgn <Skladchikov.vania@gmail.com>
2021-11-08 23:45:51 +0300

    for

---
+++ src/Part2/forTest.java
@@ -0,0 +1,10 @@
+package Part2;
+
+public class forTest {
+    public static void main(String[] args) {
+        int x;
+        for (x=0;x<10;x++) {
+            System.out.println("Значение х= " + x);
+        }
+    }
+}
