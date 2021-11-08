d727a5d462a2e3db909d474eaa2d9678669413b4
vania-sgn <Skladchikov.vania@gmail.com>
2021-11-08 23:36:32 +0300

    8

---
+++ src/Part2/Nov_8.java
@@ -0,0 +1,20 @@
+package Part2;
+
+public class Nov_8 {
+    public static void main(String[] args) {
+        int x,y ;
+         x= 10 ;
+         y= 20 ;
+        if(x<y)
+            System.out.println( "x меньше у" );
+            x=x*2;
+        if(x==y)
+            System.out.println("x == у");
+            x=x*2;
+        if(x>y)
+            System.out.println("x > у");
+            x=x*2;
+        if(x==y)
+            System.out.println("x == у");
+    }
+}
